0d508eedfa703c629d2cdef1453a684a913744cc
karlexceed <alex.decker@gmail.com>
2025-11-11 22:26:49 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,46 +3,29 @@
 
 -- Namespace
 lava_particles = {}
-
-
-local players = {}
 local scan_box = vector.new(32, 8, 32)
 
-core.register_on_joinplayer(
-	function(player, last_login)
-		table.insert(players, player:get_player_name())
-	end
-)
-
--- Update Particles
-function lava_particles.update_particles()
-	for _,player in pairs(players) do
-		local new_player_obj = core.get_player_by_name(player)
-		if (new_player_obj == nil) then break end
-		local new_pos = new_player_obj:get_pos()
-		local rounded_pos = vector.new(
-			math.floor(new_pos["x"]),
-			math.floor(new_pos["y"]),
-			math.floor(new_pos["z"])
-		)
-		local blocks = core.find_nodes_in_area(
-			rounded_pos - scan_box,
-			rounded_pos + scan_box,
-			"default:lava_source")
+-- Create particles
+function lava_particles.create_lava_particles()
+	for _,player_obj in pairs(core.get_connected_players()) do
+		if (player_obj == nil) then break end
+		local new_pos = player_obj:get_pos()
+		local rounded_pos = vector.new(math.floor(new_pos["x"]), math.floor(new_pos["y"]), math.floor(new_pos["z"]))
+		local blocks = core.find_nodes_in_area_under_air(rounded_pos - scan_box, rounded_pos + scan_box, "default:lava_source")
+		
 		for _,lpos in pairs(blocks) do
 			lnode = core.get_node(lpos)
 			lpos_above = vector.offset(lpos, 0, 1, 0)
-			lnode_above = core.get_node(lpos_above)
-			if (lnode_above.name == "air") and (math.random(1,10) == 1) then
+			if (math.random(1,40) == 1) then
 				core.add_particlespawner({
 					amount = math.random(1,3),
-					time = 2400,
+					time = 60,
 					node = {name = lnode.name},
 					object_collision = true,
 					collisiondetection = true,
 					collision_removal = true,
 					exptime = 1,
-					playername = player,
+					playername = player_obj:get_player_name(),
 					minpos = vector.offset(lpos_above, -0.5, 0, -0.5),
 					maxpos = vector.offset(lpos_above,  0.5, 0,  0.5),
 					minvel = {x = -0.8, y = 1, z = -0.8},
@@ -54,7 +37,20 @@ function lava_particles.update_particles()
 			end
 		end
 	end
-	core.after(5, lava_particles.update_particles)
 end
 
-core.after(0.2, lava_particles.update_particles)
+function lava_particles.timer(dtime)
+	lava_particles.elapsed = lava_particles.elapsed + dtime
+	if lava_particles.elapsed > lava_particles.interval then
+		lava_particles.create_lava_particles()
+		lava_particles.interval = math.random(5,30)
+		lava_particles.elapsed = 0.0
+	end
+end
+
+lava_particles.interval = math.random(5,30)
+lava_particles.elapsed = 0.0
+
+minetest.register_globalstep(function(dtime)
+	lava_particles.timer(dtime)
+end)
